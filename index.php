9734a35931f76a89cbaae9128826efbbdf565c86
Ewerton Azevedo <ewerton.azevedo@hotmail.com>
2018-09-14 19:47:42 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Cerrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("ma");
+//echo json_encode($search);
+
+//Carrega um usuario usuando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Mario", "12");
+echo $usuario;
 ?>
